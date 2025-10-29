5e6ea6fb506b7c7fcf1852f7e8beec30af6053a0
Maxwell Selassie <maxwellselassie2004@gmail.com>
2025-10-29 10:05:39 +0000

    Customer Churn Prediction Model

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,37 +1,45 @@
-
 import pandas as pd
+import numpy as np
+import logging
+from pathlib import Path
+from typing import Optional, List, Dict, Any
 import warnings
 warnings.filterwarnings('ignore')
-import logging
-from sklearn.model_selection import train_test_split
-import json
+from utils import (
+    setup_logger, get_timestamp, ensure_directories, read_yaml_file
+)
 
-logging.basicConfig(level=logging.INFO, format= '%(asctime)s - %(levelname)s : %(message)s', datefmt='%H:%M:%S')
+log = setup_logger(name='preprocessing', log_filename='logs/ preprocessing.log',level=logging.INFO)
 
-def preprocessing():
-    # load preprocessed csv file 
-    try:
-        df = pd.read_csv('../data/Preprocessed_e-commerce.csv')
-        logging.info('File opened successfully!')
-    except FileNotFoundError:
-        logging.info('File was not found! Please check filepath and try again')
+class DataPreprocesserPipeline():
+    '''Data preprocessing Pipeline driven by YAML configuration
     
-    y = df['Churn'] # traget output
-    x = df.drop(columns=['Churn','CustomerID']).copy() #drop target column from dataset
+    Workflow: 
+        - Handling missing values
+        - Handling duplicates
+        - Handling outliers
+        - Feature Scaling
+        - Featur Encoding
+        
+    '''
+    def __init__(self, config_file: str | Path) -> None:
+        '''Initialize DataPreprocessorPipeline
+        
+        Args:
+            config_file : YAML configuration file path
+            
+        '''
+        log.info("="*50)
+        log.info('INITIALIZING DATA PREPROCESSING PIPELINE')
+        log.info("="*50)
 
-    # splits the x and y datasets into training and testing sets
-    x_train,x_test,y_train,y_test = train_test_split(
-        x,y,test_size=0.3,random_state=42,stratify=y
-    )
-    # save train and test splits 
-    x_train.to_parquet('../data/x_train.parquet',index=False)
-    x_test.to_parquet('../data/x_test.parquet',index=False)
-    y_train.to_frame('y_train').to_parquet('../data/y_train.parquet',index=False)
-    y_test.to_frame('y_test').to_parquet('../data/y_test.parquet',index=False)
+        self.config = read_yaml_file(config_file)
+        self.config_file = config_file
 
-    feature_names = x_train.columns.tolist()
-    with open('../models/feature_names.json','w') as file:
-        json.dump(feature_names,file, indent=4)        
+        self.scalar = None
 
-
-    logging.info('Train/test splits and feature names successfully saved!')
+        # project metadata
+        self.log_transformations = []
+        self.metadata = {
+            
+        }
